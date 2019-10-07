bcfc4a9a7127aa64d424ed46eae8569ab0735e2b
TuzBubi <mikheev.ii@phystech.edu>
2019-10-07 18:45:49 +0300

    отпепвосил мунка

--- third lab but its munk's scream/edmunkrunnigoutfromhimself.py
+++ third lab but its munk's scream/edmunkrunnigoutfromhimself.py
@@ -1,44 +1,59 @@
 from graph import *
-from math import sin, cos, sqrt, asin
 
 
 def keyPressed(event):
     global dx, dy, ex, ey
     if event.keycode == VK_LEFT:
-        dx = -5; dy = 0
-        ex = 0; ey =0
+        dx = -5
+        dy = 0
+        ex = 0
+        ey = 0
     elif event.keycode == VK_RIGHT:
-        dx = 5; dy = 0
-        ex = 0; ey =0
+        dx = 5
+        dy = 0
+        ex = 0
+        ey = 0
     elif event.keycode == VK_UP:
-        dx = 0; dy = -5
-        ex = 0; ey =0
+        dx = 0
+        dy = -5
+        ex = 0
+        ey = 0
     elif event.keycode == VK_DOWN:
-        dx = 0; dy = 5
-        ex = 0; ey =0
+        dx = 0
+        dy = 5
+        ex = 0
+        ey = 0
     elif event.keycode == VK_TAB:
-        dx = 0; dy = 0
-        ex = -5; ey = 0
+        dx = 0
+        dy = 0
+        ex = -5
+        ey = 0
     elif event.keycode == VK_DELETE:
-        dx = 0; dy = 0
-        ex = 5; ey = 0
+        dx = 0
+        dy = 0
+        ex = 5
+        ey = 0
     elif event.keycode == VK_SPACE:
         updatemouth()  # вызов сатаны (если разкомментишь)
         print("DUUUUUUUUCK")
-        dx = 0; dy = 0
-        ex = 0; ey = 0
+        dx = 0
+        dy = 0
+        ex = 0
+        ey = 0
     elif event.keycode == VK_BACK:
         # movelefthand()  разработке
-        dx = 0; dy = 0
-        ex = 0; ey = 0
+        dx = 0
+        dy = 0
+        ex = 0
+        ey = 0
     elif event.keycode == VK_ESCAPE:
         close()
     else:
         pass
     updateeyes()
     updateman()
-    
-    
+
+
 def crosstheborder(x, y, blx, bly, bux, buy):
     global dx, dy
     
@@ -51,12 +66,12 @@ def crosstheborder(x, y, blx, bly, bux, buy):
     else:
         return (dx, dy)
 
-    
+
 def moveobs(a, x, y):
     for i in a:
         moveObjectBy(i, x, y)
 
-        
+
 def updateman():
     global dx, dy
     x = xCoord(body) + 75
@@ -69,10 +84,8 @@ def updateman():
 
 def updateeyes():
     global ex, ey
-
     xl = xCoord(leye)
     xr = xl + 20
-    ye = yCoord(leye)
     xh, yh = center(head)
     if xl < xh - 30:
         ex = 10
@@ -89,50 +102,17 @@ def updatemouth():
     mouth = circle(x, y, dm + 1, "mouth")
     dm += 1
     if dm > 10:
-        dm=4
+        dm = 4
     return
-'''
-don't move something here!
 
-def movelefthand():
-    global lhand, lh, rh, rhand
-    print(lh)
-    x=[0] * 4; y = [0] * 4
-    for i in range(4):
-        x[i], y[i] = lh[i][0], lh[i][1]
-    l = sqrt( (x[1] - x[0])*(x[1] - x[0]) + (y[1] - y[0]) * (y[1] - y[0]) )
-    a = asin( (y[0] - y[1]) / l )
-    print(cos(a))
-    a -= 0.1
-    print(cos(a))
-    xn = x[0] - l * cos(a)
-    yn = y[0] - l * sin(a)
-    x[1], y[1] = xn, yn
-    l = sqrt( (x[3] - x[0])*(x[3] - x[0]) + (y[3] - y[0]) * (y[3] - y[0]) )
-    a = asin( (y[3] - y[0]) / l )
-    a += 0.1
-    xn = x[0] + l * cos(a)
-    yn = y[0] + l * sin(a)
-    x[3], y[3] = xn, yn
-    l = sqrt( (x[2] - x[0])*(x[2] - x[0]) + (y[2] - y[0]) * (y[2] - y[0]) )
-    a = asin( (y[0] - y[2]) / l )
-    a -= 0.1
-    xn = x[0] - l * cos(a)
-    yn = y[0] - l * sin(a)
-    x[2], y[2] = xn, yn
-    deleteObject(lhand)
-    for i in range(4):
-        lh[i] = (x[i],y[i])
-    print(lh)
-    lhand=polygon(lh)
-'''
+
 dx, dy, ex, ey = 0, 0, 0, 0
 
 
 # floor
 penColor(140, 129, 46)
 brushColor(140, 129, 46)
-polygon([(0, 15), (250, 500),(0, 500)])
+polygon([(0, 15), (250, 500), (0, 500)])
 
 # sea
 penColor(36, 75, 72)
@@ -159,37 +139,32 @@ circle(400, 300, 60, "loveis")  # lower
 penSize(1)
 penColor("black")
 brushColor("black")
-body=polygon([(125, 500), (125, 300), (200, 300), (200, 500)])
+body = polygon([(125, 500), (125, 300), (200, 300), (200, 500)])
 
 # head
 brushColor(255, 168, 85)
 penColor(255, 168, 85)
-head=circle(163, 280, 30, "head")
+head = circle(163, 280, 30, "head")
 
 # mouth
-dm=4
+dm = 4
 penColor("black")
-mouth=circle(163, 290, dm, "mouth")
+mouth = circle(163, 290, dm, "mouth")
 
 # eyes
 penSize(10)
-leye=point(153, 270, "black")  # left
-reye=point(173, 270, "black")  # right
+leye = point(153, 270, "black")  # left
+reye = point(173, 270, "black")  # right
 
 # hands
 penSize(1)
 penColor("black")
 brushColor(255, 168, 85)
-lhand = polygon([ (140, 315), (130, 280), (140, 275), (150, 310)])  # left
-lh = [ (140, 315), (130, 280), (140, 275), (150, 310) ]
+lhand = polygon([(140, 315), (130, 280), (140, 275), (150, 310)])  # left
+lh = [ (140, 315), (130, 280), (140, 275), (150, 310)]
 rhand = polygon([(176, 310), (186, 315), (196, 280), (183, 275)])  # right
-rh=[(176, 310), (186, 315), (196, 280), (183, 275)]
+rh = [(176, 310), (186, 315), (196, 280), (183, 275)]
 
 onKey(keyPressed)
 
 run()
-
-
-
-
-
